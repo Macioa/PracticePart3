4d0a16ad22d5ae21493994662d4187f6be513628
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-15 13:34:19 -0500

    afternoon lesson

--- Lessons/Tuesday/tunr_django/tunr/views.py
+++ Lessons/Tuesday/tunr_django/tunr/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Artist, Song
+from .forms import ArtistForm, SongForm
+
+from rest_framework import generics
+from .serializers import ArtistSerializer, SongSerializer
+
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all().prefetch_related('artist')
+    serializer_class = SongSerializer
 
 def artist_list(request):
     artists = Artist.objects.all()
