2abf42da588b589dd7b2d112adb4502abb416bfb
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-15 20:24:05 -0500

    edits

--- Lessons/Wednesday/tunr-django-auth-master/tunr_django/tunr/views.py
+++ Lessons/Wednesday/tunr-django-auth-master/tunr_django/tunr/views.py
@@ -5,6 +5,24 @@ from .forms import ArtistForm, SongForm
 
 from django.contrib.auth.decorators import login_required
 
+from rest_framework import generics
+from .serializers import ArtistSerializer, SongSerializer
+
+class ArtistList(generics.ListCreateAPIView):
+  queryset = Artist.objects.all()
+  serializer_class = ArtistSerializer
+
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Artist.objects.all()
+  serializer_class = ArtistSerializer
+
+class SongList(generics.ListCreateAPIView):
+  queryset = Song.objects.all().prefetch_related('artist')
+  serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Song.objects.all().prefetch_related('artist')
+serializer_class = SongSerializer
 
 ############################### ARTIST ###############################
 
